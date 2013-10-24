ef1bc55ae66611b2435687096ee0f546716f0ba3
kevinbuch <kevaustinbuch@gmail.com>
2013-10-24 10:58:19 -0500

    fixed move validation

--- src/tic_tac_toe/validations.clj
+++ src/tic_tac_toe/validations.clj
@@ -2,5 +2,5 @@
   (:require [tic-tac-toe.board :refer :all]))
 
 (defn valid-move? [move board]
-  (and (contains? (vec (range 1 10)) move)
+  (and (contains? (set (range 1 10)) move)
        (= empty-space (nth board (dec move)))))
