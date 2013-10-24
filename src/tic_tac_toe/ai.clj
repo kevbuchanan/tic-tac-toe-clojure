d11d305de0e24004030a691b66dbadb0c7d9aba1
kevinbuch <kevaustinbuch@gmail.com>
2013-10-24 13:54:32 -0500

    added memoization to minimax

--- src/tic_tac_toe/ai.clj
+++ src/tic_tac_toe/ai.clj
@@ -1,16 +1,18 @@
 (ns tic-tac-toe.ai
-  (:require [tic-tac-toe.board :refer :all]))
+  (:require [tic-tac-toe.board :refer [empty-space winner draw? over? empty-board?]]))
 
 (defn possible-moves [board]
   (keep-indexed #(if (= empty-space %2) %1) board))
 
 (defn board-state-score [board piece]
-  (cond (= piece (winner board)) 1
+  (cond (= piece (winner board)) 1000
         (draw? board) 0
-        :else -1))
+        :else -1000))
 
 (defn score-factor [player]
-  (if (= player :ai) 1 -1))
+  (if (= player :ai) 1000 -1000))
+
+(declare memoized-score)
 
 (defn calculate-score [board space player piece depth]
   (let [other-piece (first (filter #(and (not= piece %) (not= empty-space %)) board))
@@ -19,15 +21,17 @@
       (if (over? moved-board)
         (* (board-state-score moved-board piece) (score-factor player) (/ 1 depth))
         (if (= player :ai)
-        (apply min (map #(calculate-score moved-board % other-player other-piece (inc depth)) (possible-moves moved-board)))
-        (apply max (map #(calculate-score moved-board % other-player other-piece (inc depth)) (possible-moves moved-board))))))))
+          (apply min (map #(memoized-score moved-board % other-player other-piece (inc depth)) (possible-moves moved-board)))
+          (apply max (map #(memoized-score moved-board % other-player other-piece (inc depth)) (possible-moves moved-board))))))))
+
+(def memoized-score (memoize calculate-score))
 
 (defn move-scores [board piece]
   (let [possibilities (possible-moves board)]
     (loop [spaces possibilities scores {}]
       (if (empty? spaces)
         scores
-        (recur (rest spaces) (assoc scores (first spaces) (calculate-score board (first spaces) :ai piece 1)))))))
+        (recur (rest spaces) (assoc scores (first spaces) (memoized-score board (first spaces) :ai piece 1)))))))
 
 (defn next-move [board piece]
   (if (empty-board? board)
