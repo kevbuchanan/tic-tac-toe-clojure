0ab08b96642784f9d193e524c968f93418677de5
kevinbuch <kevaustinbuch@gmail.com>
2013-10-29 14:21:28 -0500

    added tail recursive algorithm

--- src/tic_tac_toe/ai.clj
+++ src/tic_tac_toe/ai.clj
@@ -1,16 +1,8 @@
 (ns tic-tac-toe.ai
   (:require [tic-tac-toe.board :refer [empty-space winner draw? over? empty-board? board-size]]))
 
-(defrecord Node [board parent children move best-score])
-
-(def node-score [node]
-  (board-state-score (:board node) (:move node)))
-
-(def node-children [node]
-  (possible-boards (:board node) (:move node)))
-
-(def other-piece [node]
-  (first (filter #(and (not= (:move node) %) (not= empty-space %)) (:board node))))
+(defn other-piece [board piece]
+  (first (filter #(and (not= piece %) (not= empty-space %)) board)))
 
 (defn possible-moves [board]
   (keep-indexed #(if (= empty-space %2) %1) board))
@@ -19,29 +11,56 @@
   (let [spaces (possible-moves board)]
     (map #(assoc board % piece) spaces)))
 
-(defn board-state-score [board piece]
-  (cond (= piece (winner board)) 1000
+(defn score-factor [type]
+  (if (= type :max) 1 -1))
+
+(defn board-state-score [board piece type depth]
+  (cond (= piece (winner board)) (/ (* (score-factor type) 1) depth)
         (draw? board) 0))
 
-(defn score-factor [player]
-  (if (= player :ai) 1000 -1000))
+(defn alpha [depth]
+  (if (odd? depth) (/ -1 (inc depth)) 0))
 
-(def calculate-score (memoize (fn [node player depth]
-  (let [other-piece (other-piece node)
-        other-player (if (= player :ai) :human :ai)]
-    (if (over? moved-board)
-      (* (board-state-score moved-board piece) (score-factor player) (/ 1 depth))
-      (if (= player :ai)
-        (apply min (map #(calculate-score moved-board % other-player other-piece (inc depth)) (possible-moves moved-board)))
-        (apply max (map #(calculate-score moved-board % other-player other-piece (inc depth)) (possible-moves moved-board)))))))))
+(defrecord Node [parent board children type depth score piece])
+
+(def calculate-score (memoize (fn [node]
+  (let [alpha (alpha (:depth node))
+        board-score (board-state-score (:board node) (:piece node) (:type node) (:depth node))
+        other-piece (other-piece (:board node) (:piece node))]
+    (if (or board-score (empty? (:children node)) (and (:score node) (= (:score node) alpha)))
+      (let [score (or board-score (:score node))]
+        (if (= nil (:parent node))
+          score
+          (if (= :min (:type node))
+            (if (or (= nil (:score (:parent node))) (< score (:score (:parent node))))
+              (recur (update-in (:parent node) [:score] #(or score %)))
+              (recur (:parent node)))
+            (if (or (= nil (:score (:parent node))) (> score (:score (:parent node))))
+              (recur (update-in (:parent node) [:score] #(or score %)))
+              (recur (:parent node))))))
+      (let [next-node (Node. (update-in node [:children] rest)
+                             (first (:children node))
+                             (possible-boards (first (:children node)) (:piece node))
+                             (if (= (:type node) :max) :min :max)
+                             (inc (:depth node))
+                             nil
+                             other-piece)]
+        (recur next-node)))))))
 
 (defn move-scores [board piece]
   (let [moves (possible-moves board)
-        boards (possible-boards board piece)]
-    (loop [spaces possibilities boards boards scores {}]
+        all-boards (possible-boards board piece)]
+    (loop [spaces moves boards all-boards scores {}]
       (if (empty? spaces)
         scores
-        (recur (rest spaces) (rest boards) (assoc scores (first spaces) (calculate-score (first boards) :ai piece 1)))))))
+        (let [score (calculate-score (Node. nil
+                                            (first boards)
+                                            (possible-boards (first boards) (other-piece (first boards) piece))
+                                            :max
+                                            1
+                                            nil
+                                            piece))]
+          (recur (rest spaces) (rest boards) (assoc scores (first spaces) score)))))))
 
 (defn next-move [board piece]
   (if (empty-board? board)
