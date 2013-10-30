323b8222098622dbf1005ff85b56d480650ce425
kevinbuch <kevaustinbuch@gmail.com>
2013-10-30 10:28:55 -0500

    refactored spec helper

--- spec/tic_tac_toe/ai_spec.clj
+++ spec/tic_tac_toe/ai_spec.clj
@@ -35,27 +35,27 @@
 (describe "Going first"
 
   (it "never loses with difficulty 3"
-    (should-not-contain :lose (all-games (new-board 3) :X :ai 3)))
+    (should-not-contain :lose (all-games [(new-board 3)] [] :X 3)))
 
   (it "loses sometimes with difficulty 2"
-    (def games (all-games (new-board 3) :X :ai 2))
+    (def games (all-games [(new-board 3)] [] :X 2))
     (should-contain :draw games)
     (should-contain :win games))
 
   (it "loses more than it wins with difficulty 1"
-    (def games (all-games (new-board 3) :X :ai 1))
+    (def games (all-games [(new-board 3)] [] :X 1))
     (should-not (< (count (filter #{:lose} games)) (count (filter #{:win} games))))))
 
 (describe "Going second"
 
   (it "never loses with difficutly 3"
-    (should-not-contain :lose (all-games (new-board 3) :O :human 3)))
+    (should-not-contain :lose (all-games [(new-board 3)] [] :O 3)))
 
   (it "wins and loses with difficulty 2"
-    (def games (all-games (new-board 3) :O :human 2))
+    (def games (all-games [(new-board 3)] [] :O 2))
     (should-contain :lose games)
     (should-contain :win games))
 
   (it "loses more than it wins with difficulty 1"
-    (def games (all-games (new-board 3) :O :human 1))
+    (def games (all-games [(new-board 3)] [] :O 1))
     (should-not (< (count (filter #{:lose} games)) (count (filter #{:win} games))))))
--- spec/tic_tac_toe/spec_helper.clj
+++ spec/tic_tac_toe/spec_helper.clj
@@ -3,25 +3,26 @@
             [tic-tac-toe.ai :refer :all]
             [tic-tac-toe.board :refer :all]))
 
-(defn check-board [board]
-  (let [rows (partition 3 board)]
-  (println (nth rows 0))
-  (println (nth rows 1))
-  (println (nth rows 2))
-  (println "")
-  :lose))
+(defn find-turn [board first-move]
+  (let [x-count (count (filter #{:X} board))
+        o-count (count (filter #{:O} board))]
+    (cond (= x-count o-count) first-move
+          (> x-count o-count) :O
+          (> o-count x-count) :X)))
 
-(defn play-all-boards [board piece player difficulty]
-  (let [other-piece (if (= piece :X) :O :X)
-        other-player (if (= player :ai) :human :ai)
-        spaces (possible-moves board)]
-    (if (over? board)
-        (cond (draw? board) :draw
-            (= player :ai) :lose
-            (= player :human) :win)
-        (if (= player :ai)
-            (recur (make-move board (next-move board piece difficulty) piece) other-piece other-player difficulty)
-            (map #(play-all-boards (make-move board % piece) other-piece other-player difficulty) spaces)))))
+(defn play-all-boards [boards outcomes first-move difficulty]
+  (if (empty? boards)
+    outcomes
+    (let [board (first boards)
+          turn (find-turn board first-move)
+          other-piece (if (= turn :X) :O :X)]
+      (if (over? board)
+          (cond (draw? board) (recur (rest boards) (conj outcomes :draw) first-move difficulty)
+              (= (winner board) :X) (recur (rest boards) (conj outcomes :win) first-move difficulty)
+              (= (winner board) :O) (recur (rest boards) (conj outcomes :lose) first-move difficulty))
+          (if (= turn :X)
+              (recur (conj (rest boards) (make-move board (next-move board turn difficulty) turn)) outcomes first-move difficulty)
+              (recur (into (rest boards) (possible-boards board turn)) outcomes first-move difficulty))))))
 
-(defn all-games [board piece player difficulty]
-  (flatten (play-all-boards board piece player difficulty)))
+(defn all-games [boards outcomes first-move difficulty]
+  (flatten (play-all-boards boards outcomes first-move difficulty)))
