0a0fea0237a635f00c9b0ce62672d8540fe0c838
kevinbuch <kevaustinbuch@gmail.com>
2013-10-30 12:57:59 -0500

    added help cli option

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -6,10 +6,13 @@
 
 (defn -main [& args]
   (let [[options args banner] (cli args
-    ["-o" "--order" :default 1 :parse-fn #(Integer. %)]
-    ["-s" "--size" :default 3 :parse-fn #(Integer. %)]
-    ["-d" "--difficulty" :default 2 :parse-fn #(Integer. %)])]
-    (if (= 1 (:order options))
-      (start {:players [:human :ai] :pieces [:X :O] :board (new-board (:size options)) :difficulty (:difficulty options)})
-      (start {:players [:ai :human] :pieces [:O :X] :board (new-board (:size options)) :difficulty (:difficulty options)}))))
+    ["-h" "--help" "Show help" :flag true :default false]
+    ["-o" "--order" "1 to go first, 2 to go second" :default 1 :parse-fn #(Integer. %)]
+    ["-s" "--size" "3 to 7, for 3x3 to 7x7 board" :default 3 :parse-fn #(Integer. %)]
+    ["-d" "--difficulty" "1: easy, 2: medium, 3: hard" :default 2 :parse-fn #(Integer. %)])]
+    (if (:help options)
+      (println banner)
+      (if (= 1 (:order options))
+        (start {:players [:human :ai] :pieces [:X :O] :board (new-board (:size options)) :difficulty (:difficulty options)})
+        (start {:players [:ai :human] :pieces [:O :X] :board (new-board (:size options)) :difficulty (:difficulty options)})))))
 
