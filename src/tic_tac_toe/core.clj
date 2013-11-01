e5a84172355a0488768c739aa839c7ba0b0d7e2c
kevinbuch <kevaustinbuch@gmail.com>
2013-11-01 13:08:01 -0500

    added validations to cli options

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -10,7 +10,9 @@
     ["-o" "--order" "1 to go first, 2 to go second" :default 1 :parse-fn #(Integer. %)]
     ["-s" "--size" "3 to 7, for 3x3 to 7x7 board" :default 3 :parse-fn #(Integer. %)]
     ["-d" "--difficulty" "1: easy, 2: medium, 3: hard" :default 2 :parse-fn #(Integer. %)])]
-    (if (:help options)
+    (if (or (:help options)
+        (not (contains? #{1 2 3} (:difficulty options)))
+        (not (contains? #{3 4 5 6 7} (:size options))))
       (println banner)
       (if (= 1 (:order options))
         (start {:players [:human :ai] :pieces [:X :O] :board (new-board (:size options)) :difficulty (:difficulty options)})
